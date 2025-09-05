469505e671f58a94a2b1960258da598bb7be606f
Mirror83 <glenochieng045@gmail.com>
2025-09-05 07:36:15 +0300

    initial commit
    Just completed the driving simulation portion of the Create With Code tutorial on Unity Hub, the following commits are for the Plane Programming challenge. This commit includes the starter files for the challenge

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private readonly float speed = 5.0f; // In m/s
+    private readonly float turnSpeed = 45.0f;
+
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Move the vehicle forward
+        transform.Translate(forwardInput * Time.deltaTime * speed * Vector3.forward);
+        // Rotate the vehicle around the y-axis
+        transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * turnSpeed);
+    }
+}
