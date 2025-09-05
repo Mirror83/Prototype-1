b1baf25975224f4c7eda3f0dc34cbfbef2230753
Mirror83 <glenochieng045@gmail.com>
2025-09-05 08:04:27 +0300

    fix: make camera follow player

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+
+    private Vector3 offset = new(20f, 1.2f, 0);
 
     // Start is called before the first frame update
     void Start()
